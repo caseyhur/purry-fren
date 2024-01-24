f9cf489c9c01e1ae8766e18abe1d9b03807a7482
casey_h <caseyehurlburt@gmail.com>
2024-01-24 16:30:02 -0500

    Added if/then statements to change button color

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 var button = document.querySelector(".show-cat");
-// console.log(button);
-
 var cat = document.querySelector(".cat");
-// console.log(cat);
 
+// When user clicks button, function is triggered
 button.addEventListener("click", function () {
-    cat.classList.add("show");
-})
+    if (cat.classList.contains("show")) {
+        // console.log("yes");
+        cat.classList.remove("show");
+        button.innerText ="Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        // console.log("no");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    }
+});
